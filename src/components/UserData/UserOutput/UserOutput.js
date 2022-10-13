1c0970c57557d8af8609d4d6301514540d795cf1
GeethasriIrudayasamy <geethasriirudayasamy@gmail.com>
2022-10-13 18:44:08 +0530

    Wrapper Component

--- src/components/UserData/UserOutput/UserOutput.js
+++ src/components/UserData/UserOutput/UserOutput.js
@@ -2,9 +2,11 @@ import "./UserOutput.css";
 import Wrapper from "../Wrapper/Wrapper";
 const UserOutput = (props) => {
     return (
-        <Wrapper className="data-container">
-            <div>
-                {props.name} - {props.age}
+        <Wrapper>
+            <div className="data-container">
+                <div>
+                    {props.name} - {props.age}
+                </div>
             </div>
         </Wrapper>
     );
